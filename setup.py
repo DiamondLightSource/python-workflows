6946ef59f8945294f321fe467a16fda42d80e52e
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-10-04 13:33:28 +0100

    Add testing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='workflows',
       version='0.0',
       description='Supervised data processing in distributed environments',
-      url='http://github.com/Anthchirp/workflows',
+      url='https://github.com/Anthchirp/workflows',
       author='Markus Gerstel',
       license='BSD',
       packages=['workflows'],
---
+++ workflows/test_testing.py
@@ -0,0 +1,7 @@
+def test_testing():
+  '''A test to see if testing works.'''
+  assert False
+
+def test_success():
+  '''A test to see if testing works.'''
+  pass
