6bf3df83807da4f5c6b18d2b42f31954333a2f52
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-10-12 08:24:10 +0100

    Improve test coverage

--- workflows/contrib/start_service.py
+++ workflows/contrib/start_service.py
@@ -1,20 +1,25 @@
 from __future__ import absolute_import, division
 import sys
+import workflows
+import workflows.services
+import workflows.transport
+import workflows.frontend
+from optparse import OptionParser, SUPPRESS_HELP
 
 #
 # Starts a workflows service
 #
 
-def run(cmdline_args):
-  import workflows
-  import workflows.services
-  import workflows.transport
-  import workflows.frontend
-  from optparse import OptionParser, SUPPRESS_HELP
-
+def run(cmdline_args, program_name = 'start_service',
+        version=workflows.version()):
+  '''Example command line interface to start services.
+     :param cmdline_args: List of command line arguments to pass to parser
+     :param program_name: Name of the command line tool to display in help
+     :param version: Version number to print when run with '--version'
+  '''
   parser = OptionParser(
-    usage='start_service [options]',
-    version=workflows.version()
+    usage=program_name + ' [options]',
+    version=version
   )
   parser.add_option("-?", action="help", help=SUPPRESS_HELP)
   parser.add_option("-s", "--service", dest="service", metavar="SVC",
@@ -25,6 +30,7 @@ def run(cmdline_args):
       help="Transport mechanism. Known mechanisms: %s (default: %%default)" % \
         ", ".join(workflows.transport.get_known_transports()))
   workflows.transport.add_command_line_options(parser)
+
   (options, args) = parser.parse_args(cmdline_args)
 
   workflows.frontend.Frontend(
--- workflows/contrib/test_start_service.py
+++ workflows/contrib/test_start_service.py
@@ -1,9 +1,27 @@
 from __future__ import absolute_import, division
+import mock
 import pytest
 import workflows.contrib.start_service
 
 def test_get_command_line_help(capsys):
+  '''Running the start_service script with --help should display command line help and exit.'''
   with pytest.raises(SystemExit):
-    workflows.contrib.start_service.run(['--help'])
+    workflows.contrib.start_service.run(['--help'], program_name='sentinelvalue')
   out, err = capsys.readouterr()
-  assert 'Usage: start_service' in out
+  assert 'Usage: sentinelvalue' in out
+
+@mock.patch('workflows.contrib.start_service.OptionParser')
+@mock.patch('workflows.contrib.start_service.workflows.frontend')
+def test_script_initialises_transport_and_starts_frontend(mock_frontend, mock_parser):
+  '''Check that the start_service script sets up the transport mechanism and the frontend properly.
+     Correct service should be selected and the frontend started.'''
+  mock_options = mock.Mock()
+  mock_options.service = mock.sentinel.service
+  mock_options.transport = mock.sentinel.transport
+  mock_parser.return_value.parse_args.return_value = (mock_options, mock.Mock())
+
+  workflows.contrib.start_service.run(['-s', 'someservice'], version=mock.sentinel.version)
+
+  mock_parser.assert_called_once_with(usage=mock.ANY, version=mock.sentinel.version)
+  mock_frontend.Frontend.assert_called_once_with(service=mock.sentinel.service, transport=mock.sentinel.transport)
+  mock_frontend.Frontend.return_value.run.assert_called_once_with()
