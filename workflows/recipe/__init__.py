48db6b59b085f25ca64f6687ddcab7e02c191d50
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-10-25 14:39:55 +0100

    Add recipe base class and tests for recipe validation

--- workflows/recipe/__init__.py
+++ workflows/recipe/__init__.py
@@ -1,9 +1,68 @@
+import workflows
+
 class Recipe(object):
   '''Object containing a processing recipe that can be passed to services.
      A recipe describes how all involved services are connected together, how
      data should be passed and how errors should be handled.'''
 
+  recipe = {}
+  '''The processing recipe is encoded in this dictionary.'''
+  # TODO: Describe format
+
   def validate(self):
     '''Check whether the encoded recipe is valid. It must describe a directed
        acyclical graph, all connections must be defined, etc.'''
-    return True
+    if not self.recipe:
+      raise workflows.WorkflowsError('Invalid recipe: No recipe defined')
+
+    # Without a 'start' node nothing would happen
+    if 'start' not in self.recipe:
+      raise workflows.WorkflowsError('Invalid recipe: "start" node missing')
+    if not self.recipe['start']:
+      raise workflows.WorkflowsError('Invalid recipe: "start" node empty')
+    if not all(isinstance(x, tuple) and len(x) == 2 for x in self.recipe['start']):
+      raise workflows.WorkflowsError('Invalid recipe: "start" node invalid')
+    if any(x[0] == 'start' for x in self.recipe['start']):
+      raise workflows.WorkflowsError('Invalid recipe: "start" node points to itself')
+
+    # All other nodes must be numeric
+    nodes = filter(lambda x: not isinstance(x, int) and x != 'start', self.recipe)
+    if nodes:
+      raise workflows.WorkflowsError('Invalid recipe: Node "%s" is not numeric' % nodes[0])
+
+    # Detect cycles
+    touched_nodes = set(['start'])
+    def flatten_links(struct):
+      '''Take an output/error link object, list or dictionary and return flat list of linked nodes.'''
+      if struct is None: return []
+      if isinstance(struct, int): return [ struct ]
+      if isinstance(struct, list):
+        if not all(isinstance(x, int) for x in struct):
+          raise workflows.WorkflowsError('Invalid recipe: Invalid link in recipe (%s)' % str(struct))
+        return struct
+      if isinstance(struct, dict):
+        joined_list = []
+        for sub_list in struct.values():
+          joined_list += flatten_links(sub_list)
+        return joined_list
+      raise workflows.WorkflowsError('Invalid recipe: Invalid link in recipe (%s)' % str(struct))
+    def find_cycles(path):
+      '''Depth-First-Search helper function to identify cycles.'''
+      if path[-1] not in self.recipe:
+        raise workflows.WorkflowsError('Invalid recipe: Node "%s" is referenced via "%s" but missing' % (str(path[-1]), str(path[:-1])))
+      touched_nodes.add(path[-1])
+      node = self.recipe[path[-1]]
+      for outgoing in ('output', 'error'):
+        if outgoing in node:
+          references = flatten_links(node[outgoing])
+          for n in references:
+            if n in path:
+              raise workflows.WorkflowsError('Invalid recipe: Recipe contains cycle (%s -> %s)' % (str(path), str(n)))
+            find_cycles(path + [n])
+    for link in self.recipe['start']:
+      find_cycles(['start', link[0]])
+
+    # Test recipe for unreferenced nodes
+    for node in self.recipe:
+      if node not in touched_nodes:
+        raise workflows.WorkflowsError('Invalid recipe: Recipe contains unreferenced node "%s"' % str(node))
--- workflows/recipe/test_recipe.py
+++ workflows/recipe/test_recipe.py
@@ -1,12 +1,37 @@
+from __future__ import absolute_import, division
+import pytest
+import workflows
 import workflows.recipe
 
 def generate_recipes():
   '''Generate two recipe objects for testing.'''
   class A(workflows.recipe.Recipe):
-    recipe = { }
+    recipe = {
+        1: { 'service': 'A service',
+             'queue': 'some.queue',
+             'output': [ 2 ],
+           },
+        2: { 'service': 'B service',
+             'queue': 'another.queue',
+           },
+        'start': [
+           (1, {}),
+        ]
+      }
 
   class B(workflows.recipe.Recipe):
-    recipe = { }
+    recipe = {
+        1: { 'service': 'A service',
+             'queue': 'some.queue',
+             'output': 2,
+           },
+        2: { 'service': 'C service',
+             'queue': 'third.queue',
+           },
+        'start': [
+           (1, {}),
+        ]
+      }
 
   return A(), B()
 
@@ -17,4 +42,114 @@ def test_can_generate_recipe_objects():
   # Check that both recipies are valid
   A.validate()
   B.validate()
+
+def test_validate_tests_for_empty_recipe():
+  '''Validating a recipe that has not been defined must throw an error.'''
+  A, _ = generate_recipes()
+  A.recipe = None
+  with pytest.raises(workflows.WorkflowsError):
+    A.validate()
+
+def test_validate_tests_for_invalid_nodes():
+  '''Check that the recipe contains only numeric nodes and a non-empty 'start' node.'''
+  A, _ = generate_recipes()
+  A.recipe['xnode'] = None
+  with pytest.raises(workflows.WorkflowsError) as excinfo:
+    A.validate()
+  assert 'xnode' in excinfo.value.message
+
+  A, _ = generate_recipes()
+  del(A.recipe['start'])
+  with pytest.raises(workflows.WorkflowsError) as excinfo:
+    A.validate()
+  assert 'start' in excinfo.value.message
+
+  A.recipe['start'] = []
+  with pytest.raises(workflows.WorkflowsError) as excinfo:
+    A.validate()
+  assert 'start' in excinfo.value.message
+
+  A.recipe['start'] = [ ('something',) ]
+  with pytest.raises(workflows.WorkflowsError) as excinfo:
+    A.validate()
+  assert 'start' in excinfo.value.message
+
+  A.recipe['start'] = [ (1, 'something'), 'banana' ]
+  with pytest.raises(workflows.WorkflowsError) as excinfo:
+    A.validate()
+  assert 'start' in excinfo.value.message
+
+  A.recipe['start'] = [ (1, 'something'), ('start', 'banana') ]
+  with pytest.raises(workflows.WorkflowsError) as excinfo:
+    A.validate()
+  assert 'start' in excinfo.value.message
+
+def test_validate_tests_for_invalid_links():
+  '''Check that the nodes in recipes have valid output/error links to other nodes.'''
+
+  # Part 1: Check outgoing links from start node:
+  A, _ = generate_recipes()
+  A.recipe['start'] = [ ('asdf', None) ]
+  with pytest.raises(workflows.WorkflowsError):
+    A.validate()
+
+  A.recipe['start'] = [ (1, None), (2, None) ]
+  A.validate()
+
+  A.recipe['start'] = [ (1, None), (99, None) ]
+  with pytest.raises(workflows.WorkflowsError):
+    A.validate()
+
+  # Part 2 & 3: Check outgoing links from other nodes (output and error)
+  for outgoing in ('output', 'error'):
+    A, _ = generate_recipes()
+    A.recipe[1][outgoing] = 'asdf'
+    with pytest.raises(workflows.WorkflowsError):
+      A.validate()
+
+    A.recipe[1][outgoing] = 99
+    with pytest.raises(workflows.WorkflowsError):
+      A.validate()
+
+    A.recipe[1][outgoing] = [2, 99]
+    with pytest.raises(workflows.WorkflowsError):
+      A.validate()
+
+    A.recipe[1][outgoing] = [2, 'banana']
+    with pytest.raises(workflows.WorkflowsError):
+      A.validate()
+
+    A.recipe[1][outgoing] = { 'all': 2, 'some': [ 2, 2 ] }
+    A.validate()
+
+    A.recipe[1][outgoing] = { 'all': 2, 'some': [ 2, 99 ] }
+    with pytest.raises(workflows.WorkflowsError):
+      A.validate()
+
+  # Part 4: Check for unreferenced nodes
+  A, _ = generate_recipes()
+  A.recipe[99] = {}
+  with pytest.raises(workflows.WorkflowsError) as excinfo:
+    A.validate()
+  assert '99' in excinfo.value.message
+
+def test_validate_tests_for_cycles():
+  '''Check that validation detects cycles in recipes.  Recipes must be acyclical.'''
+  A, _ = generate_recipes()
+  A.recipe[2]['output'] = 1
+  with pytest.raises(workflows.WorkflowsError) as excinfo:
+    A.validate()
+  assert 'cycle' in excinfo.value.message
+
+  A, _ = generate_recipes()
+  A.recipe[2]['output'] = 2
+  with pytest.raises(workflows.WorkflowsError) as excinfo:
+    A.validate()
+  assert 'cycle' in excinfo.value.message
+
+  A, _ = generate_recipes()
+  A.recipe[2]['output'] = [1, 2]
+  with pytest.raises(workflows.WorkflowsError) as excinfo:
+    A.validate()
+  assert 'cycle' in excinfo.value.message
 
