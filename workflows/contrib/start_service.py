9034fdd9574e94e96ee3cd0ec364ea7b60180708
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-11-21 14:44:10 +0000

    Make the start_service command more extensible

--- workflows/contrib/start_service.py
+++ workflows/contrib/start_service.py
@@ -1,42 +1,89 @@
 from __future__ import absolute_import, division
+from optparse import OptionParser, SUPPRESS_HELP
+import sys
 import workflows
+import workflows.frontend
 import workflows.services
 import workflows.transport
-import workflows.frontend
-from optparse import OptionParser, SUPPRESS_HELP
 
-#
-# Starts a workflows service
-#
+class ServiceStarter(object):
+  '''A helper class to start a workflows service from the command line.
+     A number of hooks are provided so that this class can be subclassed and
+     used in a number of scenarios.'''
 
-def run(cmdline_args, program_name = 'start_service',
-        version=workflows.version()):
-  '''Example command line interface to start services.
-     :param cmdline_args: List of command line arguments to pass to parser
-     :param program_name: Name of the command line tool to display in help
-     :param version: Version number to print when run with '--version'
-  '''
-  parser = OptionParser(
-    usage=program_name + ' [options]',
-    version=version
-  )
-  parser.add_option("-?", action="help", help=SUPPRESS_HELP)
-  parser.add_option("-s", "--service", dest="service", metavar="SVC",
+  @staticmethod
+  def on_parser_preparation(parser):
+    '''Plugin hook to manipulate the OptionParser object before command line
+       parsing. If a value is returned here it will replace the OptionParser
+       object.'''
+
+  @staticmethod
+  def on_parsing(options, args):
+    '''Plugin hook to manipulate the command line parsing results.
+       A tuple of values can be returned, which will replace (options, args).
+       '''
+
+  @staticmethod
+  def on_frontend_preparation(frontend):
+    '''Plugin hook to manipulate the Frontend object before starting it. If a
+       value is returned here it will replace the Frontend object.'''
+
+  def parser_factory(self, program_name, version):
+    '''Create an OptionParser with pre-populated options for services etc.
+       :param program_name: Name of the command line tool to display in help
+       :param version: Version number to print when run with '--version'
+    '''
+    parser = OptionParser(
+      usage=program_name + ' [options]' if program_name else None,
+      version=version
+    )
+    parser.add_option("-?", action="help", help=SUPPRESS_HELP)
+    parser.add_option("-s", "--service", dest="service", metavar="SVC",
       default=None, help="Name of the service to start. Known services: %s" % \
         ", ".join(workflows.services.get_known_services()))
-  parser.add_option("-t", "--transport", dest="transport", metavar="TRN",
+    parser.add_option("-t", "--transport", dest="transport", metavar="TRN",
       default="StompTransport",
       help="Transport mechanism. Known mechanisms: %s (default: %%default)" % \
         ", ".join(workflows.transport.get_known_transports()))
-  workflows.transport.add_command_line_options(parser)
+    workflows.transport.add_command_line_options(parser)
 
-  (options, args) = parser.parse_args(cmdline_args)
+    self.on_parser_preparation(parser)
 
-  workflows.frontend.Frontend(
+    return parser
+
+  def run(self, cmdline_args=None, program_name='start_service',
+        version=workflows.version()):
+    '''Example command line interface to start services.
+       :param cmdline_args: List of command line arguments to pass to parser
+       :param program_name: Name of the command line tool to display in help
+       :param version: Version number to print when run with '--version'
+    '''
+
+    if cmdline_args is None:
+      cmdline_args = sys.argv[1:]
+
+    parser = self.parser_factory(program_name, version)
+
+    retval = self.on_parser_preparation(parser)
+    if retval:
+      parser = retval
+
+    (options, args) = parser.parse_args(cmdline_args)
+
+    retval = self.on_parsing(options, args)
+    if retval:
+      (options, args) = retval
+
+    frontend = workflows.frontend.Frontend(
       service=options.service,
       transport=options.transport,
-    ).run()
+    )
+
+    retval = self.on_frontend_preparation(frontend)
+    if retval:
+      frontend = retval
+
+    frontend.run()
 
 if __name__ == '__main__':  # pragma: no cover
-  import sys
-  run(sys.argv[1:])
+  ServiceStarter().run()
--- workflows/contrib/test_start_service.py
+++ workflows/contrib/test_start_service.py
@@ -6,7 +6,7 @@ import workflows.contrib.start_service
 def test_get_command_line_help(capsys):
   '''Running the start_service script with --help should display command line help and exit.'''
   with pytest.raises(SystemExit):
-    workflows.contrib.start_service.run(['--help'], program_name='sentinelvalue')
+    workflows.contrib.start_service.ServiceStarter().run(['--help'], program_name='sentinelvalue')
   out, err = capsys.readouterr()
   assert 'Usage: sentinelvalue' in out
 
@@ -20,7 +20,7 @@ def test_script_initialises_transport_and_starts_frontend(mock_frontend, mock_pa
   mock_options.transport = mock.sentinel.transport
   mock_parser.return_value.parse_args.return_value = (mock_options, mock.Mock())
 
-  workflows.contrib.start_service.run(['-s', 'someservice'], version=mock.sentinel.version)
+  workflows.contrib.start_service.ServiceStarter().run(['-s', 'someservice'], version=mock.sentinel.version)
 
   mock_parser.assert_called_once_with(usage=mock.ANY, version=mock.sentinel.version)
   mock_frontend.Frontend.assert_called_once_with(service=mock.sentinel.service, transport=mock.sentinel.transport)
