7564e35bbf9a4d09fb24326dae55d14fe987fbcf
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-11-21 09:42:19 +0000

    Add test for logging callback handler

---
+++ workflows/logging/test_logging.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import, division
+import logging
+import mock
+import workflows.logging
+
+def test_callback_handler_works_within_logging_framework():
+  cbmock = mock.Mock()
+  logmsg = 'Test message for callback'
+  log = logging.getLogger('workflows.tests.logging.callback')
+  log.setLevel(logging.INFO)
+
+  cbh = workflows.logging.CallbackHandler(cbmock)
+  log.addHandler(workflows.logging.CallbackHandler(cbmock))
+  log.info(logmsg)
+
+  cbmock.assert_called_once()
+  assert cbmock.call_args == ((mock.ANY,), {})
+  logrec = cbmock.call_args[0][0]
+  assert isinstance(logrec, logging.LogRecord)
+  assert logrec.name      == 'workflows.tests.logging.callback'
+  assert logrec.levelname == 'INFO'
+  assert logrec.message   == logmsg
+  assert logrec.funcName.startswith('test_')
