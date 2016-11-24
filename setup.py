1ee76cb7710afeadfb6d4ab9845170067200091f
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-11-24 13:18:52 +0000

    v0.20 release

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-package_version = '0.19'
+package_version = '0.20'
 
 # to release:
 #  - increment number
-#  - export NUMBER="0.19"
+#  - export NUMBER="0.20"
 #  - git add -u; git commit -m "v${NUMBER} release"; git tag -a v${NUMBER} -m v${NUMBER}; git push; git push origin v${NUMBER}
 #  - python setup.py register sdist upload
 
