106e88ab152e07381854a7e64961d17caaa79c65
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-04-10 14:49:52 +0100

    v0.38 release

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from __future__ import absolute_import, division
 from setuptools import setup, find_packages
 import sys
 
-package_version = '0.37'
+package_version = '0.38'
 
 # to release:
 #  - increment number
-#  - export NUMBER="0.37"
+#  - export NUMBER="0.38"
 #  - git add -u; git commit -m "v${NUMBER} release"; git tag -a v${NUMBER} -m v${NUMBER}; git push; git push origin v${NUMBER}
 #  - python setup.py register sdist upload
 
--- workflows/services/common_service.py
+++ workflows/services/common_service.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division
 import logging
+import threading
 import workflows
 import workflows.logging
 from workflows.transport.queue_transport import QueueTransport
@@ -137,6 +138,14 @@ class CommonService(workflows.add_plugin_register_to_class(object)):
     self._idle_callback = callback
     self._idle_time = idle_time
 
+  def _define_timer(self, interval, callback):
+    '''Create a repeating timer, that calls a callback function every
+       n seconds. This returns a timer object which offers a .cancel()
+       method to disable the timer.'''
+
+    threading.Timer(interval, callback)
+    pass
+
   def __update_service_status(self, statuscode):
     '''Set the internal status of the service object, and notify frontend.'''
     if self.__service_status != statuscode:
@@ -264,3 +273,8 @@ class CommonService(workflows.add_plugin_register_to_class(object)):
 class Commands():
   '''A list of command strings used for communicating with the frontend.'''
   SHUTDOWN = 'shutdown'
+
+class RepeatingTimer():
+  pass
+# threading.Timer(interval, callback)
+
