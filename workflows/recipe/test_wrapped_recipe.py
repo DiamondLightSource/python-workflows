8e0d5c8a74f3aab5cb341ca397bcea86d5c5754d
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-04-21 16:41:54 +0100

    Starting on the recipe wrapper send_to function

--- workflows/recipe/test_wrapped_recipe.py
+++ workflows/recipe/test_wrapped_recipe.py
@@ -2,12 +2,35 @@ from __future__ import absolute_import, division
 import mock
 import pytest
 import workflows
+from workflows.recipe import Recipe
 from workflows.recipe.wrapper import RecipeWrapper
 
 def generate_recipe_message():
   '''Helper function for tests.'''
   message = {
-     'recipe': {},
+     'recipe': {
+       1: { 'service': 'A service',
+            'queue': 'service.one',
+            'output': [ 2 ],
+            'multi-output': [ 2, 3, 4 ],
+            'error': 2,
+          },
+       2: { 'service': 'service 2',
+            'queue': 'queue.two',
+          },
+       3: { 'service': 'service 3',
+            'topic': 'topic.three',
+            'transport-delay': 300,
+          },
+       4: { 'service': 'service 4',
+            'queue': 'queue.four',
+            'transport-delay': 300,
+          },
+       'start': [
+          (1, {}),
+       ],
+       'error': [ 2 ],
+     },
      'recipe-pointer': 1,
      'recipe-path': [],
      'environment': { 'ID': mock.sentinel.GUID,
@@ -15,7 +38,7 @@ def generate_recipe_message():
                       'timestamp': mock.sentinel.timestamp,
                     },
      'payload': mock.sentinel.payload,
-     }
+    }
   return message
 
 
@@ -24,8 +47,61 @@ def test_recipe_wrapper_extracts_message_information():
 
   rw = RecipeWrapper(m)
 
-  assert rw.recipe == m['recipe']
+  assert rw.recipe == Recipe(m['recipe'])
   assert rw.recipe_pointer == m['recipe-pointer']
+  assert rw.recipe_step == m['recipe'][m['recipe-pointer']]
   assert rw.recipe_path == m['recipe-path']
   assert rw.environment == m['environment']
 
+def test_recipe_embedded_message_sending():
+  m = generate_recipe_message()
+  t = mock.create_autospec(workflows.transport.common_transport.CommonTransport)
+  def downstream_recipe(dest, payload):
+    ds_message = generate_recipe_message()
+    ds_message['recipe-pointer'] = dest
+    ds_message['recipe-path'] = [ 1 ]
+    ds_message['payload'] = payload
+    return ds_message
+
+  rw = RecipeWrapper(m, transport=t)
+  assert rw.transport == t
+  assert t.method_calls == []
+  t.reset_mock() # magic call may have been recorded
+
+  with pytest.raises(ValueError):
+    rw.send_to('unknown', mock.sentinel.message_text)
+
+  rw.send_to('output', mock.sentinel.message_text)
+
+  expected = [ mock.call.send(
+      m['recipe'][2]['queue'],
+      downstream_recipe(2, mock.sentinel.message_text),
+      header={'workflows-recipe': True},
+  ) ]
+  assert t.mock_calls == expected
+  t.reset_mock()
+
+  rw.send_to('multi-output', mock.sentinel.another_message_text, transaction=mock.sentinel.txn)
+
+  expected = []
+  expected.append(mock.call.send(
+      m['recipe'][2]['queue'],
+      downstream_recipe(2, mock.sentinel.another_message_text),
+      header={'workflows-recipe': True},
+      transaction=mock.sentinel.txn,
+  ))
+  expected.append(mock.call.broadcast(
+      m['recipe'][3]['topic'],
+      downstream_recipe(3, mock.sentinel.another_message_text),
+      header={'workflows-recipe': True},
+      transaction=mock.sentinel.txn,
+      delay=300,
+  ))
+  expected.append(mock.call.send(
+      m['recipe'][4]['queue'],
+      downstream_recipe(4, mock.sentinel.another_message_text),
+      header={'workflows-recipe': True},
+      transaction=mock.sentinel.txn,
+      delay=300,
+  ))
+  assert t.mock_calls == expected
--- workflows/recipe/wrapper.py
+++ workflows/recipe/wrapper.py
@@ -1,15 +1,68 @@
 from __future__ import absolute_import, division
 
+import copy
+import workflows.recipe
+
 class RecipeWrapper(object):
   '''A wrapper object which contains a recipe and a number of functions to make
      life easier for recipe users.
   '''
 
-  def __init__(self, message):
-    '''Create a RecipeWrapper object from a wrapped message.'''
-    self.recipe = message['recipe']
+  def __init__(self, message, transport=None, log=None):
+    '''Create a RecipeWrapper object from a wrapped message.
+       References to the transport layer are required to send directly to
+       connected downstream processes. Optionally a logger is wrappend and
+       extended to include global recipe information in log messages for
+       tracking.
+    '''
+    self.recipe = workflows.recipe.Recipe(message['recipe'])
     self.recipe_pointer = message['recipe-pointer'] # TODO: failsafe way?
+    self.recipe_step = self.recipe[self.recipe_pointer]
     self.recipe_path = message.get('recipe-path', [])
     self.environment = message.get('environment', {})
+    self.transport = transport
+    self.log = log
 
-  
+  def send_to(self, channel, message, header=None, **kwargs):
+    if not self.transport:
+      raise NotImplementedError('This RecipeWrapper object does not contain ' \
+                                'a reference to a transport object.')
+
+    if not self.recipe_step:
+      raise NotImplementedError('This RecipeWrapper object does not contain ' \
+                                'a recipe with a selected step.')
+
+    if channel not in self.recipe_step:
+      raise ValueError('The current recipe step does not have an output ' \
+                       'channel named %s.' % channel)
+
+    if not header:
+      header = {}
+    header['workflows-recipe'] = True
+
+    def generate_full_recipe_message(destination):
+      return {
+          'environment': self.environment,
+          'payload': message,
+          'recipe': self.recipe.recipe,
+          'recipe-path': self.recipe_path + [ self.recipe_pointer ],
+          'recipe-pointer': destination,
+             }
+
+    destinations = self.recipe_step[channel]
+    if not isinstance(destinations, list):
+      destinations = [ destinations ]
+    for destination in destinations:
+      dest_kwargs = kwargs.copy()
+      if 'transport-delay' in self.recipe[destination] and 'delay' not in kwargs:
+        dest_kwargs['delay'] = self.recipe[destination]['transport-delay']
+      if self.recipe[destination].get('queue'):
+        self.transport.send(
+            self.recipe[destination]['queue'],
+            generate_full_recipe_message(destination),
+            header=header, **dest_kwargs)
+      if self.recipe[destination].get('topic'):
+        self.transport.broadcast(
+            self.recipe[destination]['topic'],
+            generate_full_recipe_message(destination),
+            header=header, **dest_kwargs)
