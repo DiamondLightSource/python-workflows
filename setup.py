6d621fe62141cec460928aba318bedc296b1888a
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-06-15 10:49:42 +0100

    v0.47 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division
 from setuptools import setup, find_packages
 import sys
 
-package_version = '0.46'
+package_version = '0.47'
 
 # to release:
 #  - increment number
