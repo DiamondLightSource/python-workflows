21503408349017f840c832239ed743ca4e27c810
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-06-27 10:53:23 +0100

    Refactor utilization statistics class to be less peculiar

--- workflows/frontend/utilization.py
+++ workflows/frontend/utilization.py
@@ -3,37 +3,40 @@ from workflows.services.common_service import CommonService
 import time
 
 class UtilizationStatistics(object):
-  '''This class is not thread-safe.'''
-
-  status_history = None
+  '''Generate statistics about the percentage of time spent in different
+     statuses over a fixed time slice. This class is not thread-safe.'''
 
   def __init__(self, summation_period=10):
-    self.status_history = []
+    '''Reports will always cover the most recent period of summation_period
+       seconds.'''
     self.period = summation_period
-    self.update_status( CommonService.SERVICE_STATUS_NEW )
+    self.status_history = [ {'start': 0, 'end': None, 'status': CommonService.SERVICE_STATUS_NEW} ]
 
   def update_status(self, new_status):
+    '''Record a status change with a current timestamp.'''
     timestamp = time.time()
-    truncate = -1
-    cutoff = timestamp - self.period
-    for event in self.status_history[:-1]:
-      if event[0] > cutoff:
-        truncate = truncate + 1
-      else:
-        break
-    if truncate > 0:
-      self.status_history = self.status_history[truncate:]
-
-    self.status_history.append((timestamp, new_status))
+    self.status_history[-1]['end'] = timestamp
+    self.status_history.append({'start': timestamp, 'end': None, 'status': new_status})
 
   def report(self):
+    '''Return a dictionary of different status codes and the percentage of time
+       spent in each throughout the last summation_period seconds.
+       Truncate the aggregated history appropriately.'''
     timestamp = time.time()
+    cutoff = timestamp - self.period
+    truncate = 0
     summary = {}
-    start_times, statuses = zip(*self.status_history)
-    end_times = list(map(lambda x: x[0], self.status_history[1:])) + [ timestamp ]
-    durations = map(lambda x: x[0]-x[1], zip(end_times, start_times))
-    total_duration = sum(durations)
-    for status, duration in zip(statuses, durations):
-      summary[status] = summary.get(status, 0) + duration
+    for event in self.status_history[:-1]:
+      if event['end'] < cutoff:
+        truncate = truncate + 1
+        continue
+      summary[event['status']] = summary.get(event['status'], 0) + \
+                                 event['end'] - max(cutoff, event['start'])
+    summary[self.status_history[-1]['status']] = \
+        summary.get(self.status_history[-1]['status'], 0) + \
+        timestamp - max(cutoff, self.status_history[-1]['start'])
+    if truncate:
+      self.status_history = self.status_history[truncate:]
+    total_duration = sum(summary.values())
     summary = { s: d / total_duration for s, d in summary.items() }
     return summary
