e0bb6a0e0fe3b8973a81715dc89449f3444d5aa9
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-11-29 09:12:28 +0000

    v0.57 release
    Introduction of Python entry points

--- workflows/__init__.py
+++ workflows/__init__.py
@@ -19,7 +19,7 @@ def load_plugins(paths):
       if fid:
         fid.close()
 
-__version__ = '0.56'
+__version__ = '0.57'
 
 def version():
   '''Returns the version number of the installed workflows package.'''
