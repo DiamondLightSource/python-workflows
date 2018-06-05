5386134ffe0d6d804a57dba45ece7871389e095d
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-06-05 12:24:09 +0100

    v0.69 release

--- workflows/__init__.py
+++ workflows/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
-__version__ = '0.68'
+__version__ = '0.69'
 
 def version():
   '''Returns the version number of the installed workflows package.'''
