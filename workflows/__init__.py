d42771f77c50b51f429f6bd0234b45b64c1eae4a
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-04-09 14:27:56 +0100

    Bump version: 1.3.1 → 1.4

--- workflows/__init__.py
+++ workflows/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
-__version__ = '1.3.1'
+__version__ = '1.4'
 
 def version():
   '''Returns the version number of the installed workflows package.'''
