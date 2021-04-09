7e61b2c61bd534eecc826363fbd49722dc622c2b
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2021-04-09 11:58:50 +0100

    Bump version: 2.5 → 2.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author="Markus Gerstel",
     author_email="scientificsoftware@diamond.ac.uk",
     download_url="https://github.com/DiamondLightSource/python-workflows/releases",
-    version="2.5",
+    version="2.6",
     install_requires=["pika", "setuptools", "stomp.py<6.1.1"],
     python_requires=">=3.6",
     packages=find_packages(),
