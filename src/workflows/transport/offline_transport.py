1bece2b01b3cfbfb94a0b9e8d291dfb2e7aebe85
Irakli Sikharulidze <irakli.sikharulidze@diamond.ac.uk>
2021-10-01 13:11:42 +0100

    Add a dummy workflows transport class (#70)
    Dummy workflows transport class for tasks running in sandboxed environment disconnected from messaging infrastructure

---
+++ src/workflows/transport/offline_transport.py
@@ -0,0 +1,90 @@
+# A workflows transport that doesn't actually transport anything
+
+import json
+import logging
+import pprint
+from typing import Any, Dict
+
+from workflows.transport.common_transport import CommonTransport, json_serializer
+
+_offlog = logging.getLogger("workflows.transport.offline_transport")
+
+
+class OfflineTransport(CommonTransport):
+    """Abstraction layer for messaging infrastructure. Here we.. do nothing."""
+
+    # Add for compatibility
+    defaults: Dict[Any, Any] = {}
+    # Effective configuration
+    config: Dict[Any, Any] = {}
+
+    def __init__(self):
+        self._connected = False
+
+    def connect(self):
+        self._connected = True
+        return True
+
+    def is_connected(self):
+        return self._connected
+
+    def disconnect(self):
+        self._connected = False
+
+    def _output(self, message, details=None):
+        _offlog.info(f"Offline Transport: {message}")
+        if details:
+            _offlog.debug(details)
+
+    def broadcast_status(self, status):
+        self._output("Writing status message", pprint.pformat(status))
+
+    def _subscribe(self, sub_id, channel, callback, **kwargs):
+        self._output(
+            f"Subscribing to messages on {channel}",
+            f"subscription ID {sub_id}, callback function {callback}, further keywords: {kwargs}",
+        )
+
+    def _subscribe_broadcast(self, sub_id, channel, callback, **kwargs):
+        self._output(
+            f"Subscribing to broadcasts on {channel}",
+            f"subscription ID {sub_id}, callback function {callback}, further keywords: {kwargs}",
+        )
+
+    def _unsubscribe(self, subscription, **kwargs):
+        self._output(
+            f"Ending subscription #{subscription}", f"further keywords: {kwargs}"
+        )
+
+    def _send(
+        self, destination, message, headers=None, delay=None, expiration=None, **kwargs
+    ):
+        self._output(f"Sending {len(message)} bytes to {destination}", message)
+
+    def _broadcast(
+        self, destination, message, headers=None, delay=None, expiration=None, **kwargs
+    ):
+        self._output(f"Broadcasting {len(message)} bytes to {destination}", message)
+
+    def _transaction_begin(self, transaction_id, **kwargs):
+        self._output(f"Starting transaction {transaction_id}")
+
+    def _transaction_abort(self, transaction_id, **kwargs):
+        self._output(f"Rolling back transaction {transaction_id}")
+
+    def _transaction_commit(self, transaction_id, **kwargs):
+        self._output(f"Committing transaction {transaction_id}")
+
+    def _ack(self, message_id, subscription_id, **kwargs):
+        self._output(
+            f"Acknowledging message {message_id} in subscription {subscription_id}"
+        )
+
+    def _nack(self, message_id, subscription_id, **kwargs):
+        self._output(
+            f"Rejecting message {message_id} in subscription {subscription_id}"
+        )
+
+    @staticmethod
+    def _mangle_for_sending(message):
+        return json.dumps(message, default=json_serializer)
---
+++ tests/transport/test_offline.py
@@ -0,0 +1,439 @@
+import decimal
+import logging
+from unittest import mock
+
+import pytest
+
+import workflows.transport
+from workflows.transport.offline_transport import OfflineTransport
+
+
+def test_lookup_and_initialize_offline_transport_layer():
+    """Find the offline transport layer via the lookup mechanism and run
+    its constructor with default settings."""
+    offline = workflows.transport.lookup("OfflineTransport")
+    assert offline == OfflineTransport
+    offline()
+
+
+def test_instantiate_link_and_connect_to_broker():
+    """Test the Offline connection routine."""
+    offline = OfflineTransport()
+    assert not offline.is_connected()
+
+    offline.connect()
+    assert offline.is_connected()
+
+    offline.disconnect()
+    assert not offline.is_connected()
+
+
+def test_broadcast_status(caplog):
+    """Test the status broadcast function."""
+    offline = OfflineTransport()
+    offline.connect()
+    message = "Writing status message"
+    with caplog.at_level(logging.INFO):
+        offline.broadcast_status({"status": str(mock.sentinel.status)})
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message}",
+            )
+        ]
+    caplog.clear()
+    with caplog.at_level(logging.DEBUG):
+        offline.broadcast_status({"status": str(mock.sentinel.status)})
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message}",
+            ),
+            (
+                "workflows.transport.offline_transport",
+                logging.DEBUG,
+                f"{{'status': '{str(mock.sentinel.status)}'}}",
+            ),
+        ]
+
+
+def test_send_message(caplog):
+    """Test the message sending function."""
+    offline = OfflineTransport()
+    offline.connect()
+
+    with caplog.at_level(logging.DEBUG):
+        offline._send(str(mock.sentinel.channel), str(mock.sentinel.message))
+        message = f"Sending {len(str(mock.sentinel.message))} bytes to {str(mock.sentinel.channel)}"
+        debug = f"{str(mock.sentinel.message)}"
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message}",
+            ),
+            (
+                "workflows.transport.offline_transport",
+                logging.DEBUG,
+                debug,
+            ),
+        ]
+
+
+def test_send_broadcast(caplog):
+    """Test the broadcast sending function."""
+    offline = OfflineTransport()
+    offline.connect()
+
+    with caplog.at_level(logging.DEBUG):
+        offline._broadcast(str(mock.sentinel.channel), str(mock.sentinel.message))
+        message = f"Broadcasting {len(str(mock.sentinel.message))} bytes to {str(mock.sentinel.channel)}"
+        debug = f"{str(mock.sentinel.message)}"
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message}",
+            ),
+            (
+                "workflows.transport.offline_transport",
+                logging.DEBUG,
+                debug,
+            ),
+        ]
+
+
+def test_messages_are_serialized_for_transport(caplog):
+    """Test the message serialization."""
+    banana = {"entry": [1, 2.0, decimal.Decimal(3), "banana"]}
+    banana_str = '{"entry": [1, 2.0, 3.0, "banana"]}'
+    offline = OfflineTransport()
+    offline.connect()
+
+    with caplog.at_level(logging.DEBUG):
+        offline.send(str(mock.sentinel.channel1), banana)
+        message = f"Sending {len(banana_str)} bytes to {str(mock.sentinel.channel1)}"
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message}",
+            ),
+            (
+                "workflows.transport.offline_transport",
+                logging.DEBUG,
+                banana_str,
+            ),
+        ]
+    caplog.clear()
+    with caplog.at_level(logging.DEBUG):
+        offline.broadcast(str(mock.sentinel.channel2), banana)
+        message = (
+            f"Broadcasting {len(banana_str)} bytes to {str(mock.sentinel.channel2)}"
+        )
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message}",
+            ),
+            (
+                "workflows.transport.offline_transport",
+                logging.DEBUG,
+                banana_str,
+            ),
+        ]
+
+    with pytest.raises(Exception):
+        offline.send(str(mock.sentinel.channel), mock.sentinel.unserializable)
+
+
+def test_subscribe_to_queue(caplog):
+    """Test subscribing to a queue (producer-consumer), callback functions and unsubscribe."""
+    mock_cb1 = mock.Mock()
+    mock_cb2 = mock.Mock()
+    offline = OfflineTransport()
+    offline.connect()
+
+    with caplog.at_level(logging.INFO):
+        offline._subscribe(
+            1,
+            str(mock.sentinel.channel1),
+            mock_cb1,
+            transformation=mock.sentinel.transformation,
+        )
+        message = f"Subscribing to messages on {str(mock.sentinel.channel1)}"
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message}",
+            )
+        ]
+
+    caplog.clear()
+    with caplog.at_level(logging.DEBUG):
+        offline._subscribe(
+            1,
+            str(mock.sentinel.channel1),
+            mock_cb1,
+            transformation=mock.sentinel.transformation,
+        )
+        message = f"Subscribing to messages on {str(mock.sentinel.channel1)}"
+        debug = f"subscription ID 1, callback function {str(mock_cb1)}, further keywords: {{'transformation': {str(mock.sentinel.transformation)}}}"
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message}",
+            ),
+            ("workflows.transport.offline_transport", logging.DEBUG, f"{debug}"),
+        ]
+
+    caplog.clear()
+    with caplog.at_level(logging.INFO):
+        offline._subscribe(
+            2,
+            str(mock.sentinel.channel2),
+            mock_cb2,
+            retroactive=True,
+            selector=mock.sentinel.selector,
+            exclusive=True,
+            transformation=True,
+            priority=42,
+        )
+        message = f"Subscribing to messages on {str(mock.sentinel.channel2)}"
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message}",
+            )
+        ]
+
+    caplog.clear()
+    with caplog.at_level(logging.DEBUG):
+        offline._subscribe(
+            2,
+            str(mock.sentinel.channel2),
+            mock_cb2,
+            retroactive=True,
+            selector=mock.sentinel.selector,
+            exclusive=True,
+            transformation=True,
+            priority=42,
+        )
+        message = f"Subscribing to messages on {str(mock.sentinel.channel2)}"
+        debug = f"subscription ID 2, callback function {str(mock_cb2)}, further keywords: {{'retroactive': True, 'selector': {str(mock.sentinel.selector)}, 'exclusive': True, 'transformation': True, 'priority': 42}}"
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message}",
+            ),
+            ("workflows.transport.offline_transport", logging.DEBUG, f"{debug}"),
+        ]
+
+
+def test_subscribe_to_broadcast(caplog):
+    """Test subscribing to a topic (publish-subscribe) and callback functions."""
+    mock_cb1 = mock.Mock()
+    mock_cb2 = mock.Mock()
+    offline = OfflineTransport()
+    offline.connect()
+
+    with caplog.at_level(logging.INFO):
+        offline._subscribe_broadcast(
+            1,
+            str(mock.sentinel.channel1),
+            mock_cb1,
+            transformation=mock.sentinel.transformation,
+        )
+        message = f"Subscribing to broadcasts on {str(mock.sentinel.channel1)}"
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message}",
+            )
+        ]
+
+    caplog.clear()
+    caplog.at_level(logging.DEBUG)
+    with caplog.at_level(logging.DEBUG):
+        offline._subscribe_broadcast(
+            1,
+            str(mock.sentinel.channel1),
+            mock_cb1,
+            transformation=mock.sentinel.transformation,
+        )
+        message = f"Subscribing to broadcasts on {str(mock.sentinel.channel1)}"
+        debug = f"subscription ID 1, callback function {str(mock_cb1)}, further keywords: {{'transformation': {str(mock.sentinel.transformation)}}}"
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message}",
+            ),
+            ("workflows.transport.offline_transport", logging.DEBUG, f"{debug}"),
+        ]
+
+    caplog.clear()
+    with caplog.at_level(logging.INFO):
+        offline._subscribe_broadcast(
+            2,
+            str(mock.sentinel.channel2),
+            mock_cb2,
+            retroactive=True,
+            transformation=True,
+        )
+        message = f"Subscribing to broadcasts on {str(mock.sentinel.channel2)}"
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message}",
+            )
+        ]
+
+    caplog.clear()
+    with caplog.at_level(logging.DEBUG):
+        offline._subscribe_broadcast(
+            2,
+            str(mock.sentinel.channel2),
+            mock_cb2,
+            retroactive=True,
+            transformation=True,
+        )
+        message = f"Subscribing to broadcasts on {str(mock.sentinel.channel2)}"
+        debug = f"subscription ID 2, callback function {str(mock_cb2)}, further keywords: {{'retroactive': True, 'transformation': True}}"
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message}",
+            ),
+            ("workflows.transport.offline_transport", logging.DEBUG, f"{debug}"),
+        ]
+
+    caplog.clear()
+    with caplog.at_level(logging.INFO):
+        offline._unsubscribe(1)
+        message = "Ending subscription #1"
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message}",
+            )
+        ]
+
+    caplog.clear()
+    with caplog.at_level(logging.INFO):
+        offline._unsubscribe(2)
+        message = "Ending subscription #2"
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message}",
+            )
+        ]
+
+
+def test_transaction_calls(caplog):
+    """Test that calls to create, commit, abort transactions are properly logged."""
+    offline = OfflineTransport()
+    offline.connect()
+
+    with caplog.at_level(logging.INFO):
+        offline._transaction_begin(mock.sentinel.txid)
+        offline._send(
+            "destination", str(mock.sentinel.message), transaction=mock.sentinel.txid
+        )
+        offline._transaction_abort(mock.sentinel.txid)
+        offline._transaction_commit(mock.sentinel.txid)
+        message_start = f"Starting transaction {str(mock.sentinel.txid)}"
+        message_send = f"Sending {len(str(mock.sentinel.message))} bytes to destination"
+        message_abort = f"Rolling back transaction {str(mock.sentinel.txid)}"
+        message_commit = f"Committing transaction {str(mock.sentinel.txid)}"
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message_start}",
+            ),
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message_send}",
+            ),
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message_abort}",
+            ),
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message_commit}",
+            ),
+        ]
+
+
+def test_ack_message(caplog):
+    """Test that the _ack function is properly logged."""
+    offline = OfflineTransport()
+    offline.connect()
+
+    with caplog.at_level(logging.DEBUG):
+        offline._subscribe(1, str(mock.sentinel.channel3), None, acknowledgement=True)
+        offline._ack(str(mock.sentinel.messageid), 1)
+        message_subscribe = f"Subscribing to messages on {str(mock.sentinel.channel3)}"
+        debug = "subscription ID 1, callback function None, further keywords: {'acknowledgement': True}"
+        message_ack = (
+            f"Acknowledging message {str(mock.sentinel.messageid)} in subscription 1"
+        )
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message_subscribe}",
+            ),
+            ("workflows.transport.offline_transport", logging.DEBUG, f"{debug}"),
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message_ack}",
+            ),
+        ]
+
+
+def test_nack_message(caplog):
+    """Test that the _nack function is properly logged."""
+    offline = OfflineTransport()
+    offline.connect()
+
+    with caplog.at_level(logging.DEBUG):
+        offline._subscribe(1, str(mock.sentinel.channel3), None, acknowledgement=True)
+        offline._nack(mock.sentinel.messageid, 1)
+        message_subscribe = f"Subscribing to messages on {str(mock.sentinel.channel3)}"
+        debug = "subscription ID 1, callback function None, further keywords: {'acknowledgement': True}"
+        message_nack = (
+            f"Rejecting message {str(mock.sentinel.messageid)} in subscription 1"
+        )
+        assert caplog.record_tuples == [
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message_subscribe}",
+            ),
+            ("workflows.transport.offline_transport", logging.DEBUG, f"{debug}"),
+            (
+                "workflows.transport.offline_transport",
+                logging.INFO,
+                f"Offline Transport: {message_nack}",
+            ),
+        ]
