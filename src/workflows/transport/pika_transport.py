83c02c5f10446ea34415ba93b73a42a4e0d464ab
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2021-10-20 08:52:58 +0100

    NotImplement the correct interface

--- src/workflows/transport/pika_transport.py
+++ src/workflows/transport/pika_transport.py
@@ -516,25 +516,28 @@ class PikaTransport(CommonTransport):
             mandatory=False,
         ).result()
 
-    def _transaction_begin(self, **kwargs):
-        """Enter transaction mode.
+    def _transaction_begin(self, transaction_id, **kwargs):
+        """Start a new transaction.
+        :param transaction_id: ID for this transaction in the transport layer.
         :param **kwargs: Further parameters for the transport layer.
         """
-        raise NotImplementedError()
+        raise NotImplementedError("Transport interface not implemented")
         # self._channel.tx_select()
 
-    def _transaction_abort(self, **kwargs):
+    def _transaction_abort(self, transaction_id, **kwargs):
         """Abort a transaction and roll back all operations.
+        :param transaction_id: ID of transaction to be aborted.
         :param **kwargs: Further parameters for the transport layer.
         """
-        raise NotImplementedError()
+        raise NotImplementedError("Transport interface not implemented")
         # self._channel.tx_rollback()
 
-    def _transaction_commit(self, **kwargs):
+    def _transaction_commit(self, transaction_id, **kwargs):
         """Commit a transaction.
+        :param transaction_id: ID of transaction to be committed.
         :param **kwargs: Further parameters for the transport layer.
         """
-        raise NotImplementedError()
+        raise NotImplementedError("Transport interface not implemented")
         # self._channel.tx_commit()
 
     def _ack(
