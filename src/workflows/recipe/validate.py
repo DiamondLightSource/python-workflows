b49adb325bb3966777a844a01dbda810e485a637
Nicholas Devenish <ndevenish@gmail.com>
2026-05-11 16:16:08 +0100

    validate_recipe

--- src/workflows/recipe/validate.py
+++ src/workflows/recipe/validate.py
@@ -20,13 +20,14 @@ from __future__ import annotations
 import argparse
 import json
 import logging
+import os
 import sys
 
 import workflows
 import workflows.recipe
 
 
-def validate_recipe(json_filename):
+def validate_recipe(json_filename: str | os.PathLike[str]) -> None:
     """Reads a json file, tries to turn it into a recipe and then validates it.
     Exits on exception with non-zero error"""
 
